a8b782b07b64387a3cbb3481d5aa59296d3bd6b4
Sophika <sophika171.g@gmail.com>
2025-07-27 21:49:51 -0400

    fixed more lint suggestions

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -28,9 +28,10 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, 
-        on_delete=models.CASCADE)
+    car_make = models.ForeignKey(CarMake,
+                                on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -38,13 +39,13 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
-    model_type = models.CharField(max_length=10, 
-        choices=CAR_TYPES, default='SUV')
+    model_type = models.CharField(max_length=10,
+                                    choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+                                validators=[
+                                    MaxValueValidator(2023),
+                                    MinValueValidator(2015)
+                                ])
     # Other fields as needed
 
     def __str__(self):
--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -13,7 +13,7 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
         new_car= CarMake.objects.create(name=data['name'],
-        description= data['description'])
+                                        description= data['description'])
         car_make_instances.append(new_car)
 
     # Create CarModel instances with the corresponding CarMake instances
@@ -53,5 +53,5 @@ def initiate():
 
     for data in car_model_data:
             CarModel.objects.create(name=data['name'],
-            car_make=data['car_make'], model_type=data['type'],
-            year=data['year'])
+                                    car_make=data['car_make'], model_type=data['type'],
+                                    year=data['year'])
